351165bca2d2f694513fcab32e7f01fb52a13c96
Elisabeth Schulz <schulz.elisabeth.terra@gmail.com>
2021-12-06 17:07:26 +0100

    Day 6, gg ez

--- src/day6.rs
+++ src/day6.rs
@@ -1,3 +1,19 @@
 pub fn solve(input: &str) -> String {
-    unimplemented!()
+    let mut populations = [0u64;9];
+    for age in input.split(",").map(|str|str.trim().parse::<usize>().unwrap()) {
+        populations[age] += 1
+    }
+
+    println!("Initial {:?}", &populations);
+    for day in 0..256 {
+        let spawners = populations[0];
+        for i in 0..=7 {
+            populations[i] = populations[i + 1];
+        }
+        populations[6] += spawners;
+        populations[8] = spawners;
+
+    }
+
+    format!("total: {}",  populations.iter().cloned().sum::<u64>())
 }
