ba211d136d996939511ca3dd33d709cba81bc84d
Elisabeth Schulz <schulz.elisabeth.terra@gmail.com>
2021-12-10 13:38:11 +0100

    Day 10: Improve performance

--- src/day10.rs
+++ src/day10.rs
@@ -1,6 +1,51 @@
+use std::borrow::BorrowMut;
+use std::cell::{RefCell, UnsafeCell};
+use std::ops::Range;
+
+struct ScratchPool {
+    storage: Vec<char>,
+    watermark: usize
+}
+
+struct PoolToken(Range<usize>);
+
+impl ScratchPool {
+    fn new() -> Self {
+        let storage = Vec::with_capacity(4096);
+        let watermark = 0;
+        Self { storage, watermark }
+    }
+
+    fn push(&mut self, c: char) {
+        self.storage.push(c)
+    }
+
+    fn pop(&mut self) {
+        self.storage.pop();
+    }
+
+    fn peek(&self) -> char {
+        self.storage[self.storage.len() - 1]
+    }
+
+    fn slice(&mut self) -> PoolToken {
+        let token = PoolToken(self.watermark..self.storage.len());
+        self.watermark = self.storage.len();
+        token
+    }
+
+    fn reset(&mut self) {
+        self.storage.truncate(self.watermark);
+    }
+
+    fn view(&self, token: PoolToken)->&[char] {
+        &self.storage[token.0]
+    }
+}
+
 enum RuleViolation {
     Corrupted(char),
-    Missing(Vec<char>),
+    Missing(PoolToken),
 }
 
 fn corrupt_score(c: char) -> u32 {
@@ -23,35 +68,38 @@ fn incomplete_score(c: char) -> u64 {
     }
 }
 
-fn determine_violation(line: &str) -> RuleViolation {
-    let mut scratch = Vec::with_capacity(32);
+fn determine_violation(line: &str, scratch: &mut ScratchPool) -> RuleViolation {
     for c in line.chars() {
         match c {
             '(' | '[' | '{' | '<' => scratch.push(c),
-            ')' if *scratch.last().unwrap() == '(' => { scratch.pop(); }
-            ']' if *scratch.last().unwrap() == '[' => { scratch.pop(); }
-            '}' if *scratch.last().unwrap() == '{' => { scratch.pop(); }
-            '>' if *scratch.last().unwrap() == '<' => { scratch.pop(); }
-            _ => return RuleViolation::Corrupted(c)
+            ')' if scratch.peek() == '(' => scratch.pop(),
+            ']' if scratch.peek() == '[' =>  scratch.pop(),
+            '}' if scratch.peek() == '{' =>  scratch.pop(),
+            '>' if scratch.peek() == '<' =>  scratch.pop(),
+            _ => {
+                scratch.reset();
+                return RuleViolation::Corrupted(c)
+            }
         };
     }
 
-    scratch.reverse();
-    RuleViolation::Missing(scratch)
+    RuleViolation::Missing(scratch.slice())
 }
 
 pub fn solve(input: &str) -> String {
-    let violations = input.lines()
-        .map(|line| determine_violation(line))
-        .collect::<Vec<_>>();
+    let mut pool = ScratchPool::new();
+    let mut violations = Vec::new();
+    for line in input.lines(){
+        violations.push(determine_violation(line, &mut pool))
+    }
 
     let part1 = violations.iter().filter_map(|rv| if let RuleViolation::Corrupted(c) = rv {
         Some(corrupt_score(*c))
     } else {
         None
     }).sum::<u32>();
-    let mut part2_scores = violations.into_iter().filter_map(|rv| if let RuleViolation::Missing(v) = rv {
-        Some(v.into_iter().fold(0, |score, next| score * 5 + incomplete_score(next)))
+    let mut part2_scores = violations.into_iter().filter_map(|rv| if let RuleViolation::Missing(token) = rv {
+        Some(pool.view(token).into_iter().rev().fold(0, |score, next| score * 5 + incomplete_score(*next)))
     } else {
         None
     }).collect::<Vec<_>>();
